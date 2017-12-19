53f9398c5ac427d0febc6c774bf90772a64dff43
shilov-1@yandex.ru <Ko3e/\_oTnyLL,eHu9>
2017-12-19 10:36:26 +0300

    about page;

---
+++ src/components/about/AboutController.js
@@ -0,0 +1,167 @@
+export class AboutController {
+    constructor () {
+        this.data = [
+            //Александр Калугин
+            {
+                href: 'https://vk.com/id16835086',
+                img: '/img/about/aleksandr_kalugin.jpg',
+                name: 'Александр',
+                surname: 'Калугин'
+            },
+            //Антон Ковалевский
+            {
+                href: 'https://vk.com/id29768826',
+                img: '/img/about/anton_kovalevsky.jpg',
+                name: 'Антон',
+                surname: 'Ковалевский'
+            },
+            //Виктория Зырянова
+            {
+                href: 'https://vk.com/id14828973',
+                img: '/img/about/victory_zyryanova.jpg',
+                name: 'Виктория',
+                surname: 'Зырянова'
+            },
+            //Кирилл Непомнящий
+            {
+                href: 'https://vk.com/vospalenniy_mozg_djeka',
+                img: '/img/about/kirill_nepomnyachsiy.jpg',
+                name: 'Кирилл',
+                surname: 'Непомнящий'
+            },
+            //Наталья Бурнашкина
+            {
+                href: 'https://vk.com/id744565',
+                img: '/img/about/natalya_burnashkina.jpg',
+                name: 'Наталья',
+                surname: 'Бурнашкина'
+            },
+            //Слава Сорокин
+            {
+                href: 'https://vk.com/tafarayphoto',
+                img: '/img/about/slava_sorokin.jpg',
+                name: 'Слава',
+                surname: 'Сорокин'
+            },
+            //Тональ Нагвальевна
+            {
+                href: 'https://vk.com/id4462838',
+                img: '/img/about/tonal_nagvalievna.jpg',
+                name: 'Тональ',
+                surname: 'Нагвальевна'
+            },
+            //Дмитрий Кольдяев
+            {
+                href: 'https://vk.com/id8767',
+                img: '/img/about/dmitriy_koldyaev.jpg',
+                name: 'Дмитрий',
+                surname: 'Кольдяев'
+            },
+            //Роман Берёзин
+            {
+                href: 'https://vk.com/landschaften',
+                img: '/img/about/roman_berezin.jpg',
+                name: 'Роман',
+                surname: 'Берёзин'
+            },
+            //Виктория Чешко
+            {
+                href: 'https://vk.com/id131625031',
+                img: '/img/about/viktoria_cheshko.jpg',
+                name: 'Виктория',
+                surname: 'Чешко'
+            },
+            //София Головина
+            {
+                href: 'https://vk.com/id71689521',
+                img: '/img/about/sofia_golovina.jpg',
+                name: 'София',
+                surname: 'Головина'
+            },
+            //Георгий Тверецкий
+            {
+                href: 'https://vk.com/id3584626',
+                img: '/img/about/georgiy_tveretskiy.jpg',
+                name: 'Георгий',
+                surname: 'Тверецкий'
+            },
+            //Анна Салтыкова
+            {
+                href: 'https://vk.com/id188141450',
+                img: '/img/about/anna_saltykova.jpg',
+                name: 'Анна',
+                surname: 'Салтыкова'
+            },
+            //Анна Раевская
+            {
+                href: 'https://vk.com/id9425559',
+                img: '/img/about/anna_raevskaya.jpg',
+                name: 'Анна',
+                surname: 'Раевская'
+            },
+            //Ksenia Mirklis
+            {
+                href: 'https://vk.com/mirklis',
+                img: '/img/about/ksenia_mirklis.jpg',
+                name: 'Ksenia',
+                surname: 'Mirklis'
+            },
+            //Анна Корнеенкова
+            {
+                href: 'http://vk.com/anna_noir',
+                img: '/img/about/anna_korneenkova.jpg',
+                name: 'Анна',
+                surname: 'Корнеенкова'
+            },
+            //Юлия Елизарова
+            {
+                href: 'http://vk.com/elizarova_fot',
+                img: '/img/about/juliya_elizarova.jpg',
+                name: 'Юлия',
+                surname: 'Елизарова'
+            },
+            //Анастасия Дунаева
+            {
+                href: 'https://vk.com/id56092039',
+                img: '/img/about/anastasia_dunaeva.jpg',
+                name: 'Анастасия',
+                surname: 'Дунаева'
+            },
+            //Надежда Духанова
+            {
+                href: 'https://vk.com/id8546188',
+                img: '/img/about/nadezhda_duhanova.jpg',
+                name: 'Надежда',
+                surname: 'Духанова'
+            },
+            //Annie Autumn
+            {
+                href: 'https://vk.com/autumn_space',
+                img: '/img/about/annie_autumn.jpg',
+                name: 'Annie',
+                surname: 'Autumn'
+            },
+            //Катерина Салдаева
+            {
+                href: 'https://vk.com/awayfromlights',
+                img: '/img/about/katerina_saldaeva.jpg',
+                name: 'Катерина',
+                surname: 'Салдаева'
+            },
+            //Виктория Майонез
+            {
+                href: 'https://vk.com/id10012750',
+                img: '/img/about/viktoria_maiones.jpg',
+                name: 'Виктория',
+                surname: 'Майонез'
+            },
+            //Vladimir Deyev
+            {
+                href: 'https://vk.com/volodyalovesyou',
+                img: '/img/about/vladimir_deyev.jpg',
+                name: 'Vladimir',
+                surname: 'Deyev'
+            }
+        ]
+    }
+}
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,9 +1,10 @@
 import {app} from 'common/app/app.init';
 import template from './about.html'
 import './about.styl'
+import {AboutController} from './AboutController';
 
 app.component('about', {
     template,
-    controller: ['$scope', () => {}],
+    controller: ['$scope', AboutController],
     controllerAs: 'vm'
 });
