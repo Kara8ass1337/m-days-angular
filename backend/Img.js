a220ed686becf41afc68affbf119f8035f2409f9
shilov-1@yandex.ru <Ko3e/\_oTnyLL,eHu9>
2018-01-17 10:39:00 +0300

    convert imgs;

---
+++ backend/Img.js
@@ -0,0 +1,28 @@
+const gm = require('gm');
+
+/**
+ * class for work with imgs
+ */
+class Img {
+    constructor() {
+
+    }
+
+    /**
+     *
+     * @param path {string}
+     * @param param {string}
+     * @returns {Promise<any>}
+     */
+    static getInfo (path, param) {
+        return new Promise((resolve, reject) => {
+            gm(path)[param]((err, val) => {
+                if (err) reject(err);
+
+                resolve(val);
+            });
+        });
+    }
+}
+
+module.exports = Img;
--- backend/convertImgs.js
+++ backend/convertImgs.js
@@ -2,61 +2,119 @@ const fs = require('fs');
 const appRoot = require('app-root-path');
 const gm = require('gm');
 const readDir = require('./readDir');
+const Img = require('./Img');
 
-const imgsPath = `${appRoot}/public/img_bg`;
-const imgsDonePath = `${appRoot}/public/img_bg_done`;
-
-/**
- *
- * @param imgPath {string}
- * @param param {string}
- * @returns {Promise<any>}
- */
-function getInfo (imgPath, param) {
-    return new Promise((resolve, reject) => {
-        gm(imgPath)[param]((err, val) => {
-            if (err) reject(err);
-
-            resolve(val);
-        });
-    });
-}
-
-/**
- *
- * @param imgCur {string}
- * @returns {Promise<void>}
- */
-async function convertImg (imgCur) {
-    const imgPath = `${imgsPath}/${imgCur}`;
-    const imgInfo = {};
-
-    imgInfo.size = await getInfo(imgPath, 'size');
-    imgInfo.format = await getInfo(imgPath, 'format');
-
-    gm(imgPath).resize(600).write(`${imgsDonePath}/${imgCur}`, (err) => {
-        if (err) throw err;
-
-        return Promise.resolve();
-    });
-}
-
-function convertImgs () {
+class ConvertImgs {
     /**
      *
-     * @type {Array}
+     * @param imgsPath {string}
+     * @param imgsDonePath {string}
      */
-    const imgsList = readDir(imgsPath);
+    constructor({imgsPath, imgsDonePath} = {}) {
+        this.imgsPath = imgsPath;
+        this.imgsDonePath = imgsDonePath;
+        this.widthArr = [
+            640,
+            1280,
+            1600,
+            1920,
+            2560,
+            3840,
+            5210,
+            7680
+        ];
+    }
 
-    const promisesArr = [];
+    /**
+     * @private
+     * @param img {string}
+     * @returns {Promise<void>}
+     */
+    async prepareToConvert(img) {
+        const path = `${this.imgsPath}/${img}`;
+        const info = {};
+        const newSize = [];
 
-    imgsList.forEach((imgCur) => {
-        promisesArr.push(convertImg(imgCur.name));
-    });
+        info.size = await Img.getInfo(path, 'size');
+        info.format = await Img.getInfo(path, 'format');
 
-    Promise.all(promisesArr).then(() => {
-        console.log('done');
-    });
+        const width = info.size.width;
+
+        if (width < 640) return;
+
+        const maxWidth = ConvertImgs.getMaxWidth(width);
+
+        this.widthArr.forEach((widthCur) => {
+            if (maxWidth >= widthCur) newSize.push(widthCur);
+        });
+
+        return this.convert({
+            img: path,
+            name: img,
+            size: newSize
+        });
+    }
+
+    /**
+     * @private
+     * @param width {number}
+     */
+    static getMaxWidth(width) {
+        if (width >= 640 && width < 1280) return 640;
+        else if (width >= 1280 && width < 1600) return 1280;
+        else if (width >= 1600 && width < 1920) return 1600;
+        else if (width >= 1920 && width < 2560) return 1920;
+        else if (width >= 2560 && width < 3840) return 2560;
+        else if (width >= 3840 && width < 5210) return 3840;
+        else if (width >= 5210 && width < 7680) return 5210;
+        else if (width >= 7680) return 7680;
+    }
+
+    /**
+     * @private
+     * @param img {string}
+     * @param size[] {string}
+     * @param name {string}
+     * @returns {Promise<[any]>}
+     */
+    convert({img, size, name} = {}) {
+        const promisesArr = [];
+
+        size.forEach((sizeCur) => {
+            promisesArr.push(
+                gm(img).resize(sizeCur).write(`${this.imgsDonePath}/${sizeCur}/${name}`, (err) => {
+                    if (err) throw err;
+
+                    return Promise.resolve();
+                })
+            );
+        });
+
+        return Promise.all(promisesArr);
+    }
+
+    convertImgs() {
+        /**
+         *
+         * @type {Array}
+         */
+        const imgsList = readDir(this.imgsPath);
+
+        const promisesArr = [];
+
+        imgsList.forEach((imgCur) => {
+            promisesArr.push(this.prepareToConvert(imgCur.name));
+        });
+
+        Promise.all(promisesArr).then(() => {
+            console.log('done');
+        });
+    }
 }
 
-convertImgs();
+const convert = new ConvertImgs({
+    imgsPath: `${appRoot}/public/img_bg`,
+    imgsDonePath: `${appRoot}/public/img_bg_done`
+});
+
+convert.convertImgs();
